06c69ea3dee28ff393103600374f16c22b45b974
Vincenttainan <54768760+Vincenttainan@users.noreply.github.com>
2025-11-05 23:27:30 +0800

    Refactor movement logic and maze boundary checks
    add the maze
    let player can only move in legal places

--- main.asm
+++ main.asm
@@ -1,56 +1,141 @@
 INCLUDE Irvine32.inc
 main EQU start@0
 
+canMove PROTO,
+    xnow: DWORD,
+    ynow: DWORD
+
 .data
-    x BYTE 0
-    y BYTE 0
+    x DWORD 1
+    y DWORD 1
     msg BYTE "(x,y): ",0
     comma BYTE ", ",0
+
+    mazex DWORD 8
+    mazey DWORD 6
+    maze BYTE \
+    "########",0dh,0ah,\
+    "#      #",0dh,0ah,\
+    "# ###  #",0dh,0ah,\
+    "#   #  #",0dh,0ah,\
+    "#      #",0dh,0ah,\
+    "########",0dh,0ah,0
 
 .code
 main PROC
     call Clrscr
 
-MainLoop:
-    mov edx, OFFSET msg
-    call WriteString
-    movzx eax, x
-    call WriteDec
+    MainLoop:
+        mov edx, OFFSET msg
+        call WriteString
+        mov eax, x
+        call WriteDec
+        mov edx, OFFSET comma
+        call WriteString
+        mov eax, y
+        call WriteDec
+        call Crlf
+        
+        call ReadChar
+        cmp al, 'w'
+        je TryUp
+        cmp al, 's'
+        je TryDown
+        cmp al, 'a'
+        je TryLeft
+        cmp al, 'd'
+        je TryRight
+        jmp MainLoop
 
-    mov edx, OFFSET comma
-    call WriteString
-    movzx eax, y
-    call WriteDec
-    call Crlf
-    
-    call ReadChar
-    cmp al, 'w'
-    je MoveUp
-    cmp al, 's'
-    je MoveDown
-    cmp al, 'a'
-    je MoveLeft
-    cmp al, 'd'
-    je MoveRight
+    TryUp:
+        mov eax, x
+        mov ebx, y
+        dec ebx
+        INVOKE canMove, eax, ebx
+        cmp eax, 1
+        je MoveUp
+        jmp ClearAndLoop
 
-    jmp MainLoop
+    TryDown:
+        mov eax, x
+        mov ebx, y
+        inc ebx
+        INVOKE canMove, eax, ebx
+        cmp eax, 1
+        je MoveDown
+        jmp ClearAndLoop
 
-MoveUp:
-    dec y
-    jmp ClearAndLoop
-MoveDown:
-    inc y
-    jmp ClearAndLoop
-MoveLeft:
-    dec x
-    jmp ClearAndLoop
-MoveRight:
-    inc x
-    jmp ClearAndLoop
+    TryLeft:
+        mov eax, x
+        mov ebx, y
+        dec eax
+        INVOKE canMove, eax, ebx
+        cmp eax, 1
+        je MoveLeft
+        jmp ClearAndLoop
 
-ClearAndLoop:
-    call Clrscr
-    jmp MainLoop
+    TryRight:
+        mov eax, x
+        mov ebx, y
+        inc eax
+        INVOKE canMove, eax, ebx
+        cmp eax, 1
+        je MoveRight
+        jmp ClearAndLoop
+
+    MoveUp:
+        dec y
+        jmp ClearAndLoop
+    MoveDown:
+        inc y
+        jmp ClearAndLoop
+    MoveLeft:
+        dec x
+        jmp ClearAndLoop
+    MoveRight:
+        inc x
+        jmp ClearAndLoop
+
+    ClearAndLoop:
+        call Clrscr
+        jmp MainLoop
 
 main ENDP
+
+; ------------------------------------
+; bool canMove(xnow, ynow)
+;
+; Description:
+;   check if the player can move to position (xnow, ynow)
+;   if the target cell in the maze is not '#', movement is allowed
+;
+; Parameters:
+;   xnow - target X position
+;   ynow - target Y position
+;
+; Return:
+;   EAX = 1 : can go
+;   EAX = 0 : can't go
+; ------------------------------------
+canMove PROC USES ebx,
+    xnow: DWORD,
+    ynow: DWORD
+
+    mov eax, mazex
+    add eax, 2
+    mov ebx, ynow
+    imul eax, ebx
+    add eax, xnow
+
+    mov bl, maze[eax]
+    cmp bl, '#'
+    jne can_move_ok
+    mov eax, 0
+    ret
+
+    can_move_ok:
+        mov eax, 1
+        ret
+canMove ENDP
+
 END main
