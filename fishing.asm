c3aca8884c293f650380c5aeac42c0199d0af7c3
Vincenttainan <54768760+Vincenttainan@users.noreply.github.com>
2025-12-05 00:51:08 +0800

    Refactor fishing game input handling and loop

--- fishing.asm
+++ fishing.asm
@@ -5,6 +5,14 @@ main EQU start@0
     startMsg  BYTE "Press Space to start...", 0
     beginMsg  BYTE "Fishing Game Start", 0
 
+    lakeSiz   DWORD 50
+
+    hookSiz   DWORD 3       ; 3 <= hookSiz <= lakeSiz
+    hookPos   SDWORD 0       ; 0 <= hookPos <= lakeSiz - hookSiz
+    velocity  SDWORD 0      ; hook's velocity
+    gravity   SDWORD -1     ; + <gravity> per tick
+    jumpForce SDWORD 2      ; + <jumpForce> per tick if space is pressed
+
 .code
 main PROC
     WaitForSpace:
@@ -12,20 +20,44 @@ main PROC
         mov edx, OFFSET startMsg
         call WriteString
 
-    CheckKey:
-        call ReadKey         ; AL = char, AH = scanner
-        cmp al, ' '          ; is == space
-        jne CheckKey         ; != -> keep waiting
+    WaitKey:
+        call ReadKey
+        cmp al, ' '
+        jne WaitKey
 
-    GameStart:
         call Clrscr
         mov edx, OFFSET beginMsg
         call WriteString
 
-        ; FishGame PROC
-        ; call FishGame
-        call WaitMsg
+        mov eax, 100
+        call Delay
+
+    FishingGame:
         call Clrscr
-        exit
+        mov eax, hookPos
+        call WriteInt
+
+        mov eax, hookPos
+        add eax, gravity
+        mov hookPos, eax
+        call ReadKey
+        .IF al == ' '
+            mov eax, hookPos
+            add eax, jumpForce
+            mov hookPos, eax
+        .ENDIF
+
+        mov eax, lakeSiz
+        sub eax, hookSiz
+        .IF hookPos < 0
+            mov hookPos, 0
+        .ELSEIF hookPos > eax
+            mov hookPos, eax
+        .ENDIF
+
+        mov eax, 100
+        call Delay
+        jmp FishingGame
+    exit
 main ENDP
 END main
